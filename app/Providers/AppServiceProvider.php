22d7b142f94fc467dee90a8ddcea20b5fccda59b
yousefrz173 <yousefrazouk6@gmail.com>
2024-05-13 17:04:15 +0300

    m

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(\Illuminate\Http\Request $request)
+    {
+        
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
